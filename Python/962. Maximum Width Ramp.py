96bdea79f5463662827e9d5383751156cd2ed317
Juan Jose Sierra Mejia <juanjose.sierra@rockwellautomation.com>
2024-10-10 08:58:40 -0500

    Max index width monotonic stack solution

--- Python/962. Maximum Width Ramp.py
+++ Python/962. Maximum Width Ramp.py
@@ -1,5 +1,19 @@
 '''
+Had a previous solution that got a TLE but I got lost as I didn't understand the
+concept of a monotonic stack, so after some reading I understood we wanted the
+lowest "i" possible with the biggest "j" possibe aslong as i <= j, so with a stack
+such as [1, 2, 2, 2, ...] we only care about the "1" as any number that satisfies
+the solution with two (2 <= x) will also be satisfied with the one as 1 <= 2 and is
+the leftmost item, however if we got [2, 1, 2, 2, ...] we should consider both as
+there's a chance x >= 2, in which case 2 also satisfies the solution and it's more
+to the left than the "1".
 
+The final example that helped me understand the logic was [2, 3, 1, 2, ...], in this
+case, every case that could use the "3" can also be used by the "2" to the left and
+it will have a bigger width ramp, so we should only consider [2, 1], which means we
+can create a stack going left to right where we only append values if they're less
+than the current top of the stack. This will represent all possible starts of the
+ramp.
 '''
 
 class Solution:
